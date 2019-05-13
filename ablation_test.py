27dd914c7be0a0473d5b9bdc30a6b674e9282aec
Hemmerling <hemmerlinglab@gmail.com>
2019-05-13 15:59:43 -0700

    ablation combined fire and read into fire_and_read

--- ablation_test.py
+++ ablation_test.py
@@ -1,13 +1,8 @@
 # use 'artiq-run' command
-import sys
-import os
-import select
 from artiq.experiment import *
 import artiq.coredevice.sampler as splr
-import datetime
-import time
 import numpy as np
-import matplotlib.pyplot as plt
+import datetime
 
 # every Experiment needs a build and a run function
 class DAQ(EnvExperiment):
@@ -20,30 +15,20 @@ class DAQ(EnvExperiment):
         self.setattr_device('sampler0')
         self.setattr_argument('scope_count',NumberValue(default=600,ndecimals=0,step=1))
         self.setattr_argument('scan_count',NumberValue(default=1,ndecimals=0,step=1))
-        #self.setattr_dataset('scope_read')
-
-
-    @kernel
-    def fire_yag(self):
-        self.core.break_realtime()
-        self.ttl4.pulse(15*us)
-        delay(150*us)
-        self.ttl6.pulse(15*us)
 
     """ Tells it to run at the core device, @ kernel is necessary to run the process entirely in artiq's core
     Notes: 
     - any command that needs to talk to the computer severely slows down (or makes it not work) the process because its not in artiq's processer
     - normal python commands such as print and plotting figures slows the kernel, or will make it not work"""
     @kernel
-    def read_diode(self):
+    def fire_and_read(self):
         self.core.break_realtime()
         self.sampler0.init()
         
         """ 
-         This function sets the gain for each sampler channel according manual
-         * i iterates over 8 channels
-         * gain setting of 0 is 1
-         * gain is 10^(input setting is), which is the second number in the .set_gain_mu
+         This function sets the gain for each sampler channel
+         * i iterates over 8 adc channels
+         * gain is 10^(input setting), which is the second number in the set_gain_mu()
          --> currently has a gain of 1 """    
         for i in range(8):
             self.sampler0.set_gain_mu(i,0)
@@ -51,19 +36,20 @@ class DAQ(EnvExperiment):
         # initilization, sets up the array of zeros of values to be replaced
         data = [0]*self.scope_count
         smp = [0]*8 # array of numbers coming from each sampler port
-        # Takes the data from the scope
-        # actually doing the sampling
-        #elap = self.core.get_rtio_counter_mu()
+        
+        ### Fire and sample
+
+        self.ttl4.pulse(15*us) # trigger flash lamp
+        delay(150*us) # wait optimal time
+        self.ttl6.pulse(15*us) # trigger q-switch
         for j in range(self.scope_count):
-            delay(5.0*us)
+            delay(3.3*us) # needed to prevent underflow errors
             self.sampler0.sample_mu(smp) # reads out into smp which takes data from all 8 ports
             data[j] = smp[0] # replaces the value at specified data[j] with the updated scalar from smp[0] (channel 0 of the sampler ports)  
             
-             # needed to prevent underflow errors
-        #elap_t = self.core.mu_to_seconds(self.core.get_rtio_counter_mu() - elap)
+            
         index = range(self.scope_count)
         self.mutate_dataset('scope_read',index,data)
-        #print('Elapsed time:',elap_t)
 
     def run(self):
         self.core.reset()
@@ -71,20 +57,19 @@ class DAQ(EnvExperiment):
         self.set_dataset('scope_read',np.full(self.scope_count,np.nan))
         for i in range(self.scan_count):
             input('Press ENTER for Run {}/{}'.format(i+1,self.scan_count))
-            self.fire_yag()
-            self.read_diode()
+            self.fire_and_read()
             print('Run {}/{} Completed'.format(i+1,self.scan_count))
 
         vals = self.get_dataset('scope_read')
         volts = []
         for v in vals:
             volts.append(splr.adc_mu_to_volt(v))
-        #print(volts)
-        f_name = 'test2.txt'
+        f_name = 'test3.txt'
         f_out = open(f_name,'w')
         for v in volts:
             f_out.write(str(v)+' ')
         f_out.close()
+        print('Voltage data written to {}'.format(f_name))
 
 
 
