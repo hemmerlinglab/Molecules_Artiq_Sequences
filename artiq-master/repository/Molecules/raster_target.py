9acc3791e3a6065d596888607ef6039958a53120
Hemmerling <hemmerlinglab@gmail.com>
2019-10-15 10:40:02 -0700

    added raster sequence

---
+++ artiq-master/repository/Molecules/raster_target.py
@@ -0,0 +1,307 @@
+# use 'artiq-run' command
+from artiq.experiment import *
+import artiq.coredevice.sampler as splr
+import numpy as np
+import datetime
+import os
+import time
+import csv
+
+# every Experiment needs a build and a run function
+class Raster_Target(EnvExperiment):
+    def build(self):
+        self.setattr_device('core') # Core Artiq Device (required)
+        self.setattr_device('ttl4') # flash-lamp
+        self.setattr_device('ttl6') # q-switch
+        self.setattr_device('ttl5') # uv ccd trigger
+        self.setattr_device('sampler0') # adc voltage sampler
+        self.setattr_device('scheduler') # scheduler used
+        # EnvExperiment attribute: number of voltage samples per scan
+        self.setattr_argument('scope_count',NumberValue(default=400,unit='reads per shot',scale=1,ndecimals=0,step=1))
+        self.setattr_argument('scan_count',NumberValue(default=2,unit='averages',scale=1,ndecimals=0,step=1))
+        self.setattr_argument('step_size',NumberValue(default=60,unit='us',scale=1,ndecimals=0,step=1))
+
+        # x
+        self.setattr_argument('min_x',NumberValue(default=0.0,unit='',scale=1,ndecimals=3,step=0.001))
+        self.setattr_argument('max_x',NumberValue(default=6.0,unit='',scale=1,ndecimals=3,step=0.001))
+        self.setattr_argument('steps_x',NumberValue(default=10,unit='',scale=1,ndecimals=1,step=1))
+        
+        # y
+        self.setattr_argument('min_y',NumberValue(default=0.0,unit='',scale=1,ndecimals=3,step=0.001))
+        self.setattr_argument('max_y',NumberValue(default=6.0,unit='',scale=1,ndecimals=3,step=0.001))
+        self.setattr_argument('steps_y',NumberValue(default=10,unit='',scale=1,ndecimals=1,step=1))
+
+        ## set circle
+        #self.setattr_argument('radius',NumberValue(default=0.0,unit='',scale=1,ndecimals=3,step=0.001))
+        #self.setattr_argument('x',NumberValue(default=0.0,unit='',scale=1,ndecimals=3,step=0.001))
+        #self.setattr_argument('y',NumberValue(default=0.0,unit='',scale=1,ndecimals=3,step=0.001))
+
+        self.setattr_argument('pmt_slice_min',NumberValue(default=5,unit='ms',scale=1,ndecimals=1,step=0.1))
+        self.setattr_argument('pmt_slice_max',NumberValue(default=6,unit='ms',scale=1,ndecimals=1,step=0.1))
+        self.setattr_argument('slice_min',NumberValue(default=5,unit='ms',scale=1,ndecimals=1,step=0.1))
+        self.setattr_argument('slice_max',NumberValue(default=6,unit='ms',scale=1,ndecimals=1,step=0.1))
+
+    ### Script to run on Artiq
+    # Basic Schedule:
+    # 1) Trigger YAG Flashlamp
+    # 2) Wait 150 us
+    # 3) Trigger Q Switch
+    # 4) In parallel, read off 2 diodes and PMT
+    @kernel
+    def fire_and_read(self):
+        self.core.break_realtime() # sets "now" to be in the near future (see Artiq manual)
+        self.sampler0.init() # initializes sampler device
+        
+        ### Set Channel Gain 
+        for i in range(8):
+            self.sampler0.set_gain_mu(i,0) # (channel,setting) gain is 10^setting
+        
+        delay(260*us)
+        
+        ### Data Variable Initialization
+        data0 = [0]*self.scope_count # signal data
+        data1 = [0]*self.scope_count # fire check data
+        data2 = [0]*self.scope_count # uhv data
+        data3 = [0]*self.scope_count # post select, checks spec blue
+        data4 = [0]*self.scope_count # post select, checks slow blue
+        smp = [0]*8 # individual sample
+
+        ### Fire and sample
+        # self.ttl4.pulse(15*us) # trigger flash lamp
+        # delay(135*us) # wait optimal time (see Minilite manual)
+        # self.ttl6.pulse(15*us) # trigger q-switch
+        
+        with parallel:
+            
+            with sequential:
+                delay(150*us)
+                self.ttl4.pulse(15*us) # trigger flash lamp
+                delay(135*us) # wait optimal time (see Minilite manual)
+                self.ttl6.pulse(15*us) # trigger q-switch
+                delay(100*us) # wait until some time after green flash
+                self.ttl5.pulse(15*us) # trigger uv ccd
+            with sequential:
+                for j in range(self.scope_count):
+                    self.sampler0.sample_mu(smp) # (machine units) reads 8 channel voltages into smp
+                    data0[j] = smp[0]
+                    data1[j] = smp[1]
+                    data2[j] = smp[2]
+                    data3[j] = smp[3]
+                    data4[j] = smp[4]
+                    #delay(5*us)
+                    delay(self.step_size*us) # plus 9us from sample_mu
+
+        
+        ### Allocate and Transmit Data
+        # index = range(self.scope_count)
+        # self.mutate_dataset('absorption',index,data0)
+        # self.mutate_dataset('fire_check',index,data1)
+        self.set_dataset('absorption',(data0),broadcast=True) # class dataset for Artiq communication
+        self.set_dataset('fire_check',(data1),broadcast=True) # class dataset for Artiq communication
+        self.set_dataset('pmt',(data2),broadcast=True)
+        self.set_dataset('spec_check',(data3),broadcast=True)
+        self.set_dataset('slow_check',(data4),broadcast=True)
+
+
+    def run(self):
+        ### Initilizations
+        self.core.reset() # Initializes Artiq (required)
+        # self.set_dataset('absorption',np.full(self.scope_count,np.nan)) # class dataset for Artiq communication
+        # self.set_dataset('fire_check',np.full(self.scope_count,np.nan)) # class dataset for Artiq communication
+
+        scan_x_interval = np.linspace(self.min_x, self.max_x, self.steps_x)
+        scan_y_interval = np.linspace(self.min_y, self.max_y, self.steps_y)
+
+        self.setpoint_count = len(scan_x_interval) * len(scan_y_interval)
+
+        set_freqs = [] # absorption signal
+        volts = [] # absorption signal
+        frchks = [] # yag fire check
+        fluor = [] # fluorescence pmt signal
+        postsel = [] # spec blue post select
+        postsel2 = [] # slow blue post select
+        avgs = [0]*self.setpoint_count
+        pmt_avgs = [0]*self.setpoint_count
+        self.set_dataset('spectrum',(avgs),broadcast=True)
+        self.set_dataset('pmt_spectrum',(pmt_avgs),broadcast=True)
+        
+        # Define scan parameters
+        
+        #scan_count = 9 # number of loops/averages
+        #scan_offset = 383.949702 # THz
+        #no_of_points = 100
+
+
+        (mesh_X, mesh_Y) = np.meshgrid(scan_x_interval, scan_y_interval)
+        mesh_X = mesh_X.flatten()
+        mesh_Y = mesh_Y.flatten()
+
+
+        self.set_dataset('position_x',(mesh_X),broadcast=True)
+        self.set_dataset('position_y',(mesh_Y),broadcast=True)
+
+        self.set_dataset('times',(np.linspace(0,(self.step_size+9)*(self.scope_count-1)/1e3,self.scope_count)),broadcast=True)
+        # End of define scan parameters
+
+
+
+        my_today = datetime.datetime.today()
+
+        datafolder = '/home/molecules/software/data/'
+        setpoint_filename = '/home/molecules/skynet/Logs/setpoint.txt'
+
+        basefolder = str(my_today.strftime('%Y%m%d')) # 20190618
+        # create new folder if doesn't exist yet
+        if not os.path.exists(datafolder + basefolder):
+            os.makedirs(datafolder + basefolder)
+
+        basefilename = datafolder + basefolder + '/' + str(my_today.strftime('%Y%m%d_%H%M%S')) # 20190618_105557
+        print('Filename: ' + basefilename)
+        #save run configuration
+        conf_file = open(basefilename+'_conf','w')
+        conf_str = basefilename+'\n'
+        conf_str += 'Number of Samples per Shot: '+str(self.scope_count)+'\n'
+        conf_str += 'Number of Averages: '+str(self.scan_count)+'\n'
+        conf_str += 'Step Size: '+str(self.step_size)+' us\n'
+        conf_file.write(conf_str)
+        conf_file.close()
+        print('Config File Written')
+
+        for n, nu in enumerate(mesh_X): 
+            print('-'*30)
+            print('Setpoint {}/{}'.format(n+1,self.setpoint_count))
+
+            print(mesh_X)
+            print(n)
+            print('Setting x/y position to ' + str(mesh_X[n]) + '/' + str(mesh_Y[n]))
+
+            # move mirrors
+            print('moving mirror')
+
+
+            new_avg = 0
+            new_avg_pmt = 0
+
+            time.sleep(1)
+
+            #if n == 0:
+            #    for cntdwn in range(10):
+            #        print('Firing in {}...'.format(10-cntdwn))
+            #        time.sleep(1)
+            #    print('FIRE IN THE HOLE!!!')            
+
+            # run scan_count averages
+        
+            ### Run Experiment
+            for i in range(self.scan_count):
+                self.scheduler.pause()
+                shot_fired = False
+                blue_on = False # spec
+                slow_on = False # slowing
+
+                while not shot_fired and not blue_on:
+                    #break  #break will break out of the infinite while loop
+    #                input('Press ENTER for Run {}/{}'.format(i+1,scan_count))
+                    self.fire_and_read() # fires yag and reads voltages
+                    vals = self.get_dataset('absorption')
+                    chks = self.get_dataset('fire_check')
+                    pmts = self.get_dataset('pmt')
+                    psel = self.get_dataset('spec_check')
+                    psel2 = self.get_dataset('slow_check')
+
+                    hlp = []
+                    for v in vals:
+                        hlp.append(splr.adc_mu_to_volt(v))
+
+                    hlp2 = []
+                    for f in chks:
+                        hlp2.append(splr.adc_mu_to_volt(f))
+
+                    hlp3 = []
+                    for p in pmts:
+                        hlp3.append(splr.adc_mu_to_volt(p))
+
+                    hlp4 = []
+                    for ps in psel:
+                        hlp4.append(splr.adc_mu_to_volt(ps))
+
+                    hlp5 = []
+                    for ps2 in psel:
+                        hlp5.append(splr.adc_mu_to_volt(ps2))
+                    blue_min = splr.adc_mu_to_volt(40)
+                    slow_min = splr.adc_mu_to_volt(40)
+
+                    # check if Yag fired
+                    if np.max(np.array(hlp2)) > 0.3:
+                        # save set points for each shot
+                        if np.min(np.array(hlp4)) > blue_min:
+                                set_freqs.append(nu)
+                                volts.append(hlp)
+                                frchks.append(hlp2)
+                                fluor.append(hlp3)
+                                postsel.append(hlp4)
+                                postsel2.append(hlp5)
+                                new_avg = new_avg + sum(hlp[int(self.slice_min*1e3/self.step_size):int(self.slice_max*1e3/self.step_size)])
+                                new_avg_pmt = new_avg_pmt + sum(hlp3[int(self.pmt_slice_min*1e3/self.step_size):int(self.pmt_slice_max*1e3/self.step_size)])
+
+                                print('Scan {}/{} Completed'.format(i+1,self.scan_count))
+                                shot_fired = True
+                                blue_on = True
+                                slow_on = True
+                        else:
+                            blue_on = True#False
+                            print('Repeat shot. No Spec Blue.')
+                    else:
+                        #break
+                        # repeat shot
+                        shot_fired = True#False
+                        print('Repeat shot. No Yag.')
+                
+                    time.sleep(1)
+
+            #new_avg = new_avg/self.scan_count
+            self.mutate_dataset('spectrum',n,new_avg)
+            self.mutate_dataset('pmt_spectrum',n,new_avg_pmt)
+
+        # transform into numpy arrays                
+        freqs = np.array(set_freqs)
+        ch1 = np.array(volts)
+        ch2 = np.array(frchks)
+        ch3 = np.array(fluor)
+        ch4 = np.array(postsel)
+        ch5 = np.array(postsel2)
+
+        #print(freqs)
+
+        print('Saving data ...')
+        ### Write Data to Files
+        f_freqs = open(basefilename + '_freqs','w')
+        f_ch1 = open(basefilename + '_ch1','w')
+        f_ch2 = open(basefilename + '_ch2','w')
+        f_ch3 = open(basefilename + '_ch3','w')
+        f_ch4 = open(basefilename + '_ch4','w')
+        f_ch5 = open(basefilename + '_ch5','w')
+
+        np.savetxt(f_freqs, freqs, delimiter=",")
+        f_freqs.close()
+
+        np.savetxt(f_ch1, ch1, delimiter=",")
+        f_ch1.close()
+
+        np.savetxt(f_ch2, ch2, delimiter=",")
+        f_ch2.close()
+
+        np.savetxt(f_ch3, ch3, delimiter=",")
+        f_ch3.close()
+
+        np.savetxt(f_ch4, ch4, delimiter=",")
+        f_ch4.close()
+
+        np.savetxt(f_ch5, ch5, delimiter=",")
+        f_ch5.close()
+
+        print('Filename: ' + basefilename)
+
+        conf_file = open(basefilename+'_conf','a')
+        conf_file.write('RUN FINISHED')
+        conf_file.close()
--- python_server/test_client.py
+++ python_server/test_client.py
@@ -6,7 +6,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 #server_address = ('localhost', 10000)
-server_address = ('192.168.42.30', 10000)
+server_address = ('192.168.42.20', 62000)
 print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
