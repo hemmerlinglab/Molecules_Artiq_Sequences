02c4dddcd0adcadfa038274f9cc63d2c96b4cccc
Hemmerling <hemmerlinglab@gmail.com>
2019-10-14 18:55:36 -0700

    added connex client

---
+++ python_server/connex_client.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+#server_address = ('localhost', 10000)
+server_address = ('192.168.42.20', 62000)
+print('connecting to %s port %s' % server_address)
+sock.connect(server_address)
+
+try:
+    
+    # Send data
+    message = '4.20/4.25' # 3.5 - 4.5/4.25 and 4.0/3.0 - 5.0
+    print('sending "%s"' % message)
+    sock.sendall(message.encode())
+
+    
+
+    ## Look for the response
+    #amount_received = 0
+    #amount_expected = len(message)
+    
+    #while amount_received < amount_expected:
+    #    data = sock.recv(16)
+    #    amount_received += len(data)
+    #    print('received "%s"' % data)
+
+finally:
+    print('closing socket')
+    sock.close()
+
+
+
