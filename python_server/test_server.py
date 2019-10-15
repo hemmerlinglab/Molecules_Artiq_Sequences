cd12ce13fce69bfcc46e2f732504cb97535ce38e
hemmerlinglab <hemmerlinglab@gmail.com>
2019-10-14 17:23:50 -0700

    got it to work

--- python_server/test_server.py
+++ python_server/test_server.py
@@ -6,7 +6,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('192.168.42.20', 62000)
 print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
