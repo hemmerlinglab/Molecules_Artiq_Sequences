79b13a24cc123e118a5e16568d45bca568b2544f
Hemmerling <hemmerlinglab@gmail.com>
2020-08-07 18:57:04 -0700

    update

--- artiq-master/repository/scan_test.py
+++ artiq-master/repository/scan_test.py
@@ -43,33 +43,30 @@ def run_seq(ext, laser_freq, wavemeter_offset, calib_freq):
             
     freq_offset_str = "{0:10.6f}".format(laser_freq/1e12)
  
-    os.system('artiq_run -q Molecules/Scan_Single_Laser_Socket.py extension=' + ext + ' scan_count=10 setpoint_count=20 setpoint_min=-600 setpoint_max=800 which_scanning_laser=1 offset_laser1=' + freq_offset_str  + '  wavemeter_offset=' + wm_str + ' hene_calibration=' + hene_calib_str + ' yag_check=True blue_check=True')
-
+    os.system('artiq_run -q Molecules/Scan_Single_Laser_Socket.py extension=' + ext + ' scan_count=20 setpoint_count=30 setpoint_min=-275 setpoint_max=275 which_scanning_laser=1 offset_laser1=' + freq_offset_str  + '  wavemeter_offset=' + wm_str + ' hene_calibration=' + hene_calib_str + ' yag_check=True blue_check=True')
 
 
 
 if len(sys.argv) == 2:
+
+    laser_freq_Q00 = 382.110400e12
+    laser_freq_R00 = 382.115147e12
+    hene_freq = 473.612512e12
+    
+    channel = 1
+
     if sys.argv[1] == 'init':
 
-        channel = 1
-
-        hene_freq = 473.612512e12
-        laser_freq = 382.110450e12
+        laser_freq = laser_freq_R00
 
         calibrate(channel, hene_freq/1e12)
-        
         init_freq(channel, laser_freq/1e12)
-
 
     if sys.argv[1] == 'run':
         
         # loop over calibration frequencies
 
-        channel = 1
-
-        hene_freq = 473.612512e12
-        
-        calib_freq_arr = np.linspace(hene_freq - 300e6, hene_freq + 300e6, 10)
+        calib_freq_arr = np.linspace(hene_freq - 300e6, hene_freq + 300e6, 5)
         
         #calib_freq_arr = [hene_freq]
         
@@ -80,21 +77,18 @@ if len(sys.argv) == 2:
             calibrate(channel, calib_freq/1e12)
         
             # wavemeter_offset (MHz)
-            #wavemeter_offset = (calib_freq - hene_freq)/1e6
-            
             wavemeter_offset = 0.8 * (calib_freq - hene_freq)/1e6 - 18.0
         
             print('Running scan over R00 - 35')
-            #laser_freq = 382.115147e12
-            laser_freq = 382.110400e12
+            laser_freq = laser_freq_R00
 
             run_seq('100' + str(n), laser_freq, wavemeter_offset, calib_freq)
             
             #time.sleep(2)
-            #
+            
             #print('Running scan over R00 - 37')
             #laser_freq = 382.117097e12
-            #
+            
             #run_seq('200' + str(n), laser_freq, wavemeter_offset, calib_freq)
                    
             # scan over rubidium lines
