15d234a7d22dba7fdf90831b743d4177fc094804
Hemmerling <hemmerlinglab@gmail.com>
2023-06-05 11:22:25 -0700

    update

---
+++ python_server/Keysight_Spectrum_Analyzer/keysight.py
@@ -0,0 +1,229 @@
+import socket
+import time
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+class Keysight:
+    
+    def __init__(self):
+
+        TCP_IP = '192.168.42.63'
+        TCP_PORT = 5025
+
+        self.command_delay = 0.01
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+        s.connect((TCP_IP,TCP_PORT))
+    
+        s.send(b"*IDN?\n")
+        t = s.recv(1024)
+    
+        #print(t)
+        
+        self.socket = s
+    
+
+    def send(self, msg):
+
+        send_msg = msg + '\n'
+
+        self.socket.send(send_msg.encode())
+
+        time.sleep(self.command_delay)
+
+    def recv(self):
+
+        self.msg = self.socket.recv(1024)
+
+        return self.msg
+
+    def query(self, msg, number_of_bytes = 1024):
+
+        self.send(msg)
+
+        self.msg = self.socket.recv(number_of_bytes)
+
+        self.msg = self.msg.decode().strip('\n')
+
+        return self.msg
+
+
+    def set_center_freq(self, freq):
+
+        self.send(':FREQ:CENT ' + str(freq) + ' Hz')
+
+    def set_span(self, span):
+
+        self.send(':FREQ:SPAN ' + str(span) + ' Hz')
+
+    def marker_on(self, no):
+
+        self.send(':CALC:MARK' + str(no) + ':STATE ON')
+
+    def get_trace(self):
+
+        #print('Getting trace')
+        #
+        #
+        ##my_format = (self.query('FORMAT:TRACE:DATA?'))
+
+        #print(no_of_points)
+        ##print(my_format)
+
+        ##d = self.query(':TRACE:DATA? TRACE1', number_of_bytes = 17*no_of_points)
+
+        no_of_points = int(self.query('SENS:SWE:POIN?'))
+        
+        #print(no_of_points)
+        no_of_points = 821
+
+        d = self.query(':TRACE:DATA? TRACE1', number_of_bytes = 17*no_of_points)
+        
+        d = d + self.query(':TRACE:DATA? TRACE1', number_of_bytes = 17*no_of_points)
+        
+        d = d.strip(',')
+       
+        #print(len(d)/17)
+        d2 = np.array([float(x) for x in d.split(',')])
+        
+        return d2[:-1]
+
+
+    def set_sweep(self):
+
+        
+        self.send(':TRACE:CLE TRACE1')
+
+        self.send(':INIT:CONT OFF')
+        
+        self.query('*OPC?')
+       
+        self.send(':INIT:IMM')
+        
+        self.query('*OPC?')
+        
+        return 
+
+
+
+    def marker_measure(self, no, wait_time = None):
+
+        self.send(':CALC:MARK' + str(no) + ':MAX')
+
+        if not wait_time is None:
+            time.sleep(wait_time)
+
+        x = self.query(':CALC:MARK' + str(no) + ':X?')
+        
+        y = self.query(':CALC:MARK' + str(no) + ':Y?')        
+        
+        err = self.query(':SYST:ERR?')        
+
+        return (np.float64(x), np.float64(y), err)
+
+    def close(self):
+
+        self.socket.close()
+
+
+
+
+
+
+
+
+
+
+
+
+x_arr = []
+y_arr = []
+
+
+
+
+low_freq = 10e6 
+high_freq = 250e6 
+high_freq = 210e6
+
+
+no_of_scans = 200
+
+span_freq = high_freq - low_freq
+cnt_freq = (high_freq + low_freq)/2.0
+
+
+results = []
+for k in range(no_of_scans):
+    try:
+        spec = Keysight()
+
+        spec.set_center_freq(cnt_freq)
+        spec.set_span(span_freq)
+
+
+        spec.set_sweep()
+        d = spec.get_trace()
+        spec.close()
+
+        if len(d) == 1000:
+            results.append(d)
+    except:
+        pass
+
+results = np.array(results)
+
+no_of_scans = results.shape[0]
+
+#no = 3
+#
+#f = open('spec_data_x_' + str(no) + '.csv', 'w')
+#np.savetxt(f, x_arr, delimiter=",")
+#f.close()
+#
+#f = open('spec_data_y_' + str(no) + '.csv', 'w')
+#np.savetxt(f, y_arr, delimiter=",")
+#f.close()
+
+xscale = np.linspace(low_freq, high_freq, 1001) / 1e6
+
+xscale = xscale[:-1]
+
+
+
+
+f = open('freq_drift_x.csv', 'w')
+
+np.savetxt(f, xscale, delimiter = ',')
+
+f.close()
+
+f = open('freq_drift_y.csv', 'w')
+
+np.savetxt(f, results, delimiter = ',')
+
+f.close()
+
+
+
+
+
+
+plt.figure()
+
+
+plt.subplot(2,1,1)
+plt.pcolor(xscale, np.arange(no_of_scans), results)
+plt.subplot(2,1,2)
+plt.plot(xscale, results[0, :])
+
+plt.show()
+
+
+
+
+
+
