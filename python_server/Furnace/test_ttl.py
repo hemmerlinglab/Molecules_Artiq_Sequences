c5c92fbc13b2b4861e73f8f8e901620d24c5b613
Hemmerling <hemmerlinglab@gmail.com>
2023-11-17 14:35:19 -0800

    update

---
+++ python_server/Furnace/test_ttl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import time
+
+import RPi.GPIO as io # using RPi.GPIO
+
+io.setmode(io.BCM)
+
+io.setup(4,io.OUT) # make pin into an output
+
+print("LED blinker - By Mike Cook")
+
+print("Ctrl C to quit")
+
+while True:
+
+io.output(4,0)
+
+time.sleep(0.30)
+
+io.output(4,1)
+
+time.sleep(0.30)
+
+
