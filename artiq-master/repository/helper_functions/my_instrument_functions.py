6d0fecf8b1ff2c4a7232655d29db8cebd51a1da0
Hemmerling <hemmerlinglab@gmail.com>
2025-08-05 09:21:00 -0700

    update

--- artiq-master/repository/helper_functions/my_instrument_functions.py
+++ artiq-master/repository/helper_functions/my_instrument_functions.py
@@ -208,7 +208,6 @@ def get_wavemeter_readings():
 
     #    data = sock.recv(len_msg)
 
-    print('asd')
     # 'request' gets only one frequency
     try:    
         # Request data
@@ -222,8 +221,6 @@ def get_wavemeter_readings():
 
     finally:
         sock.close()
-
-    print('done')
 
     # return a list of freqs
     # currently only one frequency is returned
