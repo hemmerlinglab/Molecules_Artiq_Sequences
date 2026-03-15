aaf21556a0a2d403f526055170b177fe27880d37
Boerge Hemmerling <boerge@169-231-35-95.wireless.ucsb.edu>
2026-03-15 14:55:41 -0700

    update

---
+++ artiq-master/repository/helper_functions/atom_lines.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+#####################################################################
+
+def get_atom_data(atom = 'Rb85'):
+
+    # dictionary of atoms
+    # down to hyperfine
+    # data[atom]
+
+    # All frequencies in Hz
+    # All hyperfine splitting frequencies are relative to the fine structure
+    
+    atom_data = {
+            'Rb87' : {
+                'S1/2-P3/2' : 384.230484468e12,
+                'S1/2-P1/2' : 377.107463380e12,
+                'S1/2' : {
+                    '2' : -2.563005979e9,
+                    '1' : +4.271676631e9
+                    },
+                'P3/2' : {
+                    '3' : +193.7407e6,
+                    '2' : -72.9112e6,
+                    '1' : -229.8518e6,
+                    '0' : -302.0738e6
+                    },
+                'P1/2' : {
+                    '2' : +305.44e6,
+                    '1' : -509.06e6,
+                    }
+                },
+            'Rb85' : {
+                'S1/2-P3/2' : 384.230406373e12,
+                'S1/2-P1/2' : 377.107385690e12,
+                'S1/2' : {
+                    '3' : -1.264888516e9,
+                    '2' : +1.770843922e9
+                    },
+                'P3/2' : {
+                    '4' : +100.205e6,
+                    '3' : -20.435e6,
+                    '2' : -83.835e6,
+                    '1' : -113.208e6
+                    },
+                'P1/2' : {
+                    '3' : +150.659e6,
+                    '2' : -210.923e6,
+                    }
+                }
+            }
+
+    return atom_data[atom] 
+
+
+#####################################################################
+
+def get_transition_freq(atom, Ji, Jf, Fi, Ff):
+
+    # calculates the absolute transition frequency of specific atom from |Ji, Fi> -> |Jf, Ff>
+    d = get_atom_data(atom)
+
+    electronic_transition = '{0}-{1}'.format(Ji, Jf)
+
+    # fine structure transition
+    freq_fine_structure = d[electronic_transition]
+
+    # hyperfine splitting
+    freq_HFS_i = d[Ji][Fi]
+    freq_HFS_f = d[Jf][Ff]
+
+    # add fine structure and hyperfine splitting
+    freq_abs = freq_fine_structure + freq_HFS_i + freq_HFS_f
+
+    return freq_abs
+
+
