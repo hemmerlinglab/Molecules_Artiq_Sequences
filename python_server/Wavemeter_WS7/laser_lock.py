246c36d99e6f97813068e43b24d0b02a2e0ac945
Hemmerling <hemmerlinglab@gmail.com>
2026-04-02 10:56:41 -0700

    update

--- python_server/Wavemeter_WS7/laser_lock.py
+++ python_server/Wavemeter_WS7/laser_lock.py
@@ -1,10 +1,10 @@
 import socket
 import serial
 import time
-from simple_pid import PID
 import threading
 import queue
 import numpy as np
+from simple_pid import PID
 
 from arduino_control import *
 from network_tools   import *
@@ -233,8 +233,8 @@ opts = {
         'pids' : {
             1 : {'laser' : 391,  'wavemeter_channel' : 1, 'Kp' : -10, 'Ki' :   -5000, 'arduino_no' : 0, 'DAC_chan' : 1, 'DAC_max_output' : 4095.0},
             2 : {'laser' : 398,  'wavemeter_channel' : 2, 'Kp' :  -5, 'Ki' :   -5000, 'arduino_no' : 0, 'DAC_chan' : 2, 'DAC_max_output' : 4095.0},
-			3 : {'laser' : 1046, 'wavemeter_channel' : 3, 'Kp' : -10, 'Ki' : -100000, 'arduino_no' : 1, 'DAC_chan' : 2, 'DAC_max_output' : 4095.0},
-            8 : {'laser' : 1046, 'wavemeter_channel' : 8, 'Kp' :  10, 'Ki' :  100000, 'arduino_no' : 1, 'DAC_chan' : 1, 'DAC_max_output' : 4095.0}
+			#3 : {'laser' : 1046, 'wavemeter_channel' : 3, 'Kp' : -10, 'Ki' : -100000, 'arduino_no' : 1, 'DAC_chan' : 2, 'DAC_max_output' : 4095.0},
+            #8 : {'laser' : 1046, 'wavemeter_channel' : 8, 'Kp' :  10, 'Ki' :  100000, 'arduino_no' : 1, 'DAC_chan' : 1, 'DAC_max_output' : 4095.0}
             },
         'fiber_switcher_init_channel' : 2
         }
