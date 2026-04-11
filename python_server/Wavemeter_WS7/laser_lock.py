bb972787b24495e6378ee6825250e37dbb476062
hemmerlinglab <49076552+hemmerlinglab@users.noreply.github.com>
2026-04-11 10:17:36 -0700

    update

--- python_server/Wavemeter_WS7/laser_lock.py
+++ python_server/Wavemeter_WS7/laser_lock.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
             'fiber_server_port'     : 65000,
             'pids' : {
                 1 : {'laser' : 391,  'wavemeter_channel' : 1, 'Kp' : -10, 'Ki' :   -5000, 'arduino_no' : 0, 'DAC_chan' : 1, 'DAC_max_output' : 4095.0},
-                2 : {'laser' : 398,  'wavemeter_channel' : 2, 'Kp' :  -5, 'Ki' :   -5000, 'arduino_no' : 0, 'DAC_chan' : 2, 'DAC_max_output' : 4095.0},
+                #2 : {'laser' : 398,  'wavemeter_channel' : 2, 'Kp' :  -5, 'Ki' :   -5000, 'arduino_no' : 0, 'DAC_chan' : 2, 'DAC_max_output' : 4095.0},
     			#3 : {'laser' : 1046, 'wavemeter_channel' : 3, 'Kp' : -10, 'Ki' : -100000, 'arduino_no' : 1, 'DAC_chan' : 2, 'DAC_max_output' : 4095.0},
                 #8 : {'laser' : 1046, 'wavemeter_channel' : 8, 'Kp' :  10, 'Ki' :  100000, 'arduino_no' : 1, 'DAC_chan' : 1, 'DAC_max_output' : 4095.0}
                 },
