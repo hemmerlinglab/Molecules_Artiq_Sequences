f5b19532a4afe226c58a0bba2cedcbc222f76608
hemmerlinglab <49076552+hemmerlinglab@users.noreply.github.com>
2026-04-13 10:05:47 -0700

    update

--- python_server/Wavemeter_Lock/run_wavemeter.bat
+++ python_server/Wavemeter_Lock/run_wavemeter.bat
@@ -1,12 +1,9 @@
 
 
 
-wt -w0 sp powershell python c:\Users\user\Desktop\Molecules_Artiq_Sequences\python_server\Wavemeter_WS7\wavemeter_fiber_switch_server.py
+wt -w0 sp powershell python3.14 c:\Users\hemme\Molecules_Artiq_Sequences\python_server\Wavemeter_Lock\wavemeter_fiber_switch_server.py
 
-wt -w0 sp powershell python c:\Users\user\Desktop\Molecules_Artiq_Sequences\python_server\Wavemeter_WS7\laser_lock.py
+wt -w0 sp powershell python3.14 c:\Users\hemme\Molecules_Artiq_Sequences\python_server\Wavemeter_Lock\laser_lock.py
 
-wt -w0 sp powershell python c:\Users\user\Desktop\Molecules_Artiq_Sequences\python_server\Wavemeter_WS7\laser_gui.py
-
-
-
+wt -w0 sp powershell python3.14 c:\Users\hemme\Molecules_Artiq_Sequences\python_server\Wavemeter_Lock\laser_gui.py
 
